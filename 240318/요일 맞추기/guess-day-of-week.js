4099bc4a2355aa400842724fdac483a813b82e34
In Hwan KIM <dev_210@naver.com>
2024-03-18 13:54:27 +0900

    Title: 요일 맞추기, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240318/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+let week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+let year = 2011;
+let date1 = new Date(`${year}-${m1}-${d1}`);
+let date2 = new Date(`${year}-${m2}-${d2}`);
+
+const diff = Math.ceil((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
+const answer = diff > 0 ? week[diff % 7] : week[week.length  + (diff % 7)];
+
+console.log(answer);
