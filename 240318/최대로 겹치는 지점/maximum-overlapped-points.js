3e7433fce3a1b12a3ff67eb56e308017d32349dd
In Hwan KIM <dev_210@naver.com>
2024-03-18 20:14:13 +0900

    Title: 최대로 겹치는 지점, Time: 176ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240318/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = Array(200).fill(0);
+for(let i = 1; i <= n; i++){
+    const [x1, x2] = input[i].split(' ').map(x=>parseInt(x)+100);
+    for(let j = x1; j <= x2; j++){
+        arr[j] += 1;
+    }
+}
+
+const answer = Math.max(...arr);
+console.log(answer);
