7fbedc995b0d34e2c897d6548571fb791adcc4b6
In Hwan KIM <dev_210@naver.com>
2024-03-18 21:15:34 +0900

    Title: 왔다 갔던 구역 2, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240318/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 240318/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -8,18 +8,11 @@ let index = 0;
 for(let i = 1; i <= n; i++){
     const [x, dir] = input[i].split(' ');
     const nx = parseInt(x);
-    
-    if(dir === 'R'){
-        for(let j = index; j < index + nx; j++){
-            visitied[j] = (visitied[j] || 0) + 1;
-        }
-        index += nx;
-    } else if(dir === 'L'){
-        for(let j = index; j > index - nx; j--){
-            visitied[j] = (visitied[j] || 0) + 1;
-        }
-        index -= nx;
-    } 
+
+    for(let j = 0; j < nx; j++){
+        index += dir === 'R' ? 1 : -1;
+        visitied[index] = (visitied[index] || 0) + 1;
+    }
 }
 
 const answer = Object.values(visitied).filter(x=>x > 1).length;
