64de56c3f9b676e598835681b6baf376ad16a571
In Hwan KIM <dev_210@naver.com>
2024-03-18 23:51:59 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 259ms, Memory: 53MB, Status: Wrong Answer - CodeTree

--- 240318/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 240318/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -3,22 +3,33 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const arr = Array.from({length:2001}, x=>Array(2001).fill(0));
 
+let minx = 2000;
+let miny = 2000;
+let maxx = 0;
+let maxy = 0;
+
 for(let i = 0; i < 2; i++){
     const [a,b,c,d] = input[i].split(' ').map(Number);
     let [x1, y1, x2, y2] = [a+1000, b+1000, c+1000, d+1000];
-    x1 < x2 ? null : [x1, x2] = [x2, x1];
-    y1 < y2 ? null : [y1, y2] = [y2, y1];
     
     for(let j = y1; j < y2; j++){
         for(let k = x1; k < x2; k++){
             if(i===0){
                 arr[j][k] += 1;
+                minx = Math.min(minx, x1);
+                miny = Math.min(miny, y1);
+
             } else {
                 arr[j][k] = 0;
+            }
+
+            if(arr[j][k] !== 0){
+                maxx = Math.max(maxx, k);
+                maxy = Math.max(maxy, j);
             }
         }
     }
 }
 
-const answer = arr.map(x=>x.filter(y=>y>0).length).filter(x=>x!==0);
-console.log(Math.max(...answer)*answer.length);
+const answer = (maxx - minx + 1) * (maxy - miny + 1);
+console.log(answer)
