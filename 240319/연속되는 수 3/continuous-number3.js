2bef39fa167cce5612ee1b9e00ba26521440ae34
In Hwan KIM <dev_210@naver.com>
2024-03-19 13:39:30 +0900

    Title: 연속되는 수 3, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240319/연속되는 수 3/continuous-number3.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = [];
+
+for(let i = 1; i <= n; i++){
+    arr.push(parseInt(input[i]));
+}
+
+let index = 0;
+let count = [];
+for(let j = 0; j < n; j++){
+    if(j === 0 || (arr[j] > 0 && arr[j-1] > 0) || (arr[j] < 0 && arr[j-1] < 0)){
+        count[index] ? count[index] += 1 : count[index] = 1;
+    } else {
+        index++
+        count[index] ? count[index] += 1 : count[index] = 1;
+    }
+}
+
+console.log(Math.max(...count))
