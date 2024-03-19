644fc63fbb655dac6ffac03c42873662342c4312
In Hwan KIM <dev_210@naver.com>
2024-03-19 11:21:47 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 175ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -7,37 +7,15 @@ const rect2 = input[1].split(' ').map(Number);
 function calc(rect1, rect2) {
     let [x1, y1, x2, y2] = rect1;
     let [x3, y3, x4, y4] = rect2;
-    let minRect = (x2 - x1) * (y2 - y1);
+    
+    // 겹치는 부분의 좌표를 계산
+    const overlapX = Math.max(0, Math.min(x2, x4) - Math.max(x1, x3));
+    const overlapY = Math.max(0, Math.min(y2, y4) - Math.max(y1, y3));
 
-    if(y3 <= y1 && y2 <= y4){
-        if(x3 <= x2 && x2 <= x4){
-            minRect = (x3 - x1) * (y2 - y1);
-        }
+    const area = (x2 - x1) * (y2 - y1);
+    const overlapArea = overlapX * overlapY;
 
-        if(x4 <= x2 && x3 <= x1){
-            minRect = (x2 - x4) * (y2 - y1);
-        }
-        
-        if(x3 <= x1 && x2 <= x4){
-            minRect = 0;
-        }
-    }
-
-    if(x3 <= x1 && x2 <= x4){
-        if(y3 <= y2 && y2 <= y4){
-            minRect = (y3 - y1) * (x2 - x1);
-        }
-
-        if(y4 <= y2 && y3 <= y1){
-            minRect = (y2 - y4) * (x2 - x1);
-        }
-        
-        if(y3 <= y1 && y2 <= y4){
-            minRect = 0;
-        }
-    }
-
-    return minRect;
+    return area - overlapArea;
 }
 
 const answer = calc(rect1, rect2);
