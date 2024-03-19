cc6e580d8d48701ea5085562c2ab0ff5d6fb3251
In Hwan KIM <dev_210@naver.com>
2024-03-19 13:09:13 +0900

    Title: 연속되는 수 2, Time: 251ms, Memory: 47MB, Status: Passed - CodeTree

--- 240319/연속되는 수 2/continuous-number2.js
+++ 240319/연속되는 수 2/continuous-number2.js
@@ -8,13 +8,15 @@ for(let i = 1; i <= n; i++){
     arr.push(parseInt(input[i]));
 }
 
-let count = 0;
+let index = 0;
+let count = [];
 for(let j = 0; j < n; j++){
     if(j === 0 || arr[j] === arr[j-1]){
-        count++;
+        count[index] ? count[index] += 1 : count[index] = 1;
     } else {
-        count = 1;
+        index++
+        count[index] ? count[index] += 1 : count[index] = 1;
     }
 }
 
-console.log(count)
+console.log(Math.max(...count))
