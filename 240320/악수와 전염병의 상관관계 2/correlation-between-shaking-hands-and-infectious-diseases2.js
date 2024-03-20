5638dbdf9969ae617293a6ef54fc2c222d86d97f
In Hwan KIM <dev_210@naver.com>
2024-03-20 23:30:59 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 223ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240320/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, k, p, t] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(x=>x.trim().split(' ').map(Number));
+const sortArr = arr.sort((a,b)=>a[0] - b[0]);
+
+let result = Array(n+1).fill(0);
+result[p] = 1;
+
+let limit = Array(n+1).fill(k);
+
+for(let [t, x, y] of sortArr){
+    if(limit[x] > 0){
+        result[y] = 1;
+        limit[x] -= 1;
+    }
+}
+
+console.log(result.slice(1).join(''));
