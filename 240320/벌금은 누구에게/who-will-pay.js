d36ff44bc21daf82ec3df3a4d289b83d2da5d3a4
In Hwan KIM <dev_210@naver.com>
2024-03-20 17:22:19 +0900

    Title: 벌금은 누구에게, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/벌금은 누구에게/who-will-pay.js
+++ 240320/벌금은 누구에게/who-will-pay.js
@@ -3,14 +3,20 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [n, m, k] = input[0].split(' ').map(Number);
 
-let student = Array(n+1).fill(0);
+function solution(n, m, k){
+    let student = Array(n+1).fill(0);
+    
+    for(let i = 1; i <= m; i++){
+        const num = parseInt(input[i]);
+        student[num] += 1;
 
-for(let i = 1; i <= m; i++){
-    const num = parseInt(input[i]);
-    student[num] += 1;
-
-    if(student[num] === k){
-        console.log(num);
-        break;
+        if(student[num] === k){
+            return num;
+        }
     }
+
+    return -1;
 }
+
+const answer = solution(n, m, k)
+console.log(answer)
