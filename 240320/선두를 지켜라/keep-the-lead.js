58f9a74871c6d0d36d79af491d5c8184784aa3bb
In Hwan KIM <dev_210@naver.com>
2024-03-20 17:46:34 +0900

    Title: 선두를 지켜라, Time: 242ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240320/선두를 지켜라/keep-the-lead.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+const A = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+const B = input.slice(n + 1, n + m + 1).map(line => line.split(' ').map(Number));
+
+function solution(n, m, A, B){
+    let moveA = [0];
+    let moveB = [0];
+    let timeA = 1, timeB = 1;
+    let winner = 'A';
+
+    for(let [v, t] of A){
+        for(let i = 0; i < t; i++){
+            moveA.push(moveA[timeA-1] + v);
+            timeA++;
+        }
+    }
+
+    for(let [v, t] of B){
+        for(let i = 0; i < t; i++){
+            moveB.push(moveB[timeB-1] + v);
+            timeB++;
+        }
+    }
+
+    for(let i = 1; i < moveA.length; i++){
+        if(moveA[i] !== moveB[i]){
+            winner = moveA[i] < moveB[i] ? 'B' : 'A';
+            break;
+        }
+    }
+
+    let count = 0;
+    for(let i = 1; i < moveA.length; i++){
+
+        if(winner === 'A'){
+            if(moveA[i] < moveB[i]){
+                winner = 'B';
+                count++;
+            }
+        } else {
+            if(moveB[i] < moveA[i]){
+                winner = 'A';
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+const answer = solution(n, m, A, B);
+console.log(answer)
