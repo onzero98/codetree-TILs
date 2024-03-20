b058c5bc069461900f0a8a8183d9434d2acb7b68
In Hwan KIM <dev_210@naver.com>
2024-03-20 22:24:25 +0900

    Title: 좌우로 움직이는 로봇, Time: 526ms, Memory: 66MB, Status: Passed - CodeTree

--- 240320/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
+++ 240320/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -6,8 +6,8 @@ const A = input.slice(1, n + 1).map(line => line.split(' '));
 const B = input.slice(n + 1, n + m + 1).map(line => line.split(' '));
 
 function solution(n, m, A, B){
-    let moveA = Array(1000000).fill(0);
-    let moveB = Array(1000000).fill(0);
+    let moveA = Array(1000000).fill(null);
+    let moveB = Array(1000000).fill(null);
     let timeA = 1, timeB = 1;
 
     for(let [t, d] of A){
@@ -24,8 +24,19 @@ function solution(n, m, A, B){
         }
     }
 
+    // 밟은 위치 그대로 유지 동기화
+    if (timeA < timeB) {
+        for (let i = timeA; i < timeB; i++) {
+            moveA[i] = moveA[i - 1];
+        }
+    } else if (timeA > timeB) {
+        for (let i = timeB; i < timeA; i++) {
+            moveB[i] = moveB[i - 1];
+        }
+    }
+
     let count = 0;
-    for(let i = 1; i <= Math.max(timeA, timeB); i++){
+    for(let i = 1; i < Math.max(timeA, timeB); i++){
         if(moveA[i-1] !== moveB[i-1] && moveA[i] === moveB[i]){
             count++;
         }
