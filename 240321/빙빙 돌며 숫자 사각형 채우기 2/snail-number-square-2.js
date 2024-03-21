ca71a48cb8384254c32f3058f9a45f92db04e215
In Hwan KIM <dev_210@naver.com>
2024-03-21 22:08:56 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240321/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const arr = Array.from(({length:n}), x=>Array(m).fill(0));
+
+const dx = [1, 0, -1, 0]
+const dy = [0, 1, 0, -1];
+let dir = 0;
+
+let x = 0, y = 0;
+arr[x][y] = 1;
+
+for(let i = 2; i <= n * m; i++){
+    let nx = x + dx[dir];
+    let ny = y + dy[dir];
+
+    if(!(0 <= nx && nx < n && 0 <= ny && ny < m) || arr[nx][ny] !== 0){
+        dir = (dir + 1) % 4;
+        nx = x + dx[dir];
+        ny = y + dy[dir];
+    } 
+
+    x = nx;
+    y = ny;
+    arr[x][y] = i;
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(arr[i].join(' '));
+}
