555945b912c689df1165628c6d8b4ebe53efa011
In Hwan KIM <dev_210@naver.com>
2024-03-21 10:25:04 +0900

    Title: 방향에 맞춰 이동, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240321/방향에 맞춰 이동/move-in-direction.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = input.slice(1).map((x)=>x.split(' '));
+let map = Array(2001).fill(0);
+
+const dx = [1, 0, 0, -1];
+const dy = [0, 1, -1, 0];
+const dir = ['W', 'S', 'N', 'E'];
+let sp = [1000, 1000];
+
+for(let [d, m] of arr){
+    const way = dir.indexOf(d);
+    sp[0] = sp[0] + m * dx[way];
+    sp[1] = sp[1] + m * dy[way];
+}
+
+console.log(-(sp[0]-1000), -(sp[1]-1000))
