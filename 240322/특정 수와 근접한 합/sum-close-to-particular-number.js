fbc98350d09dbf6117ed9b06e3d8214a93426148
In Hwan KIM <dev_210@naver.com>
2024-03-22 13:27:09 +0900

    Title: 특정 수와 근접한 합, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240322/특정 수와 근접한 합/sum-close-to-particular-number.js
+++ 240322/특정 수와 근접한 합/sum-close-to-particular-number.js
@@ -4,28 +4,17 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [n, s] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
-function getComb(arr, n){
-    if(n === 1)
-        return arr.map(x=> [x]);
-    
-    let res = [];
-    arr.forEach((fix, idx, origin)=>{
-        const rest = origin.slice(idx+1);
-        const comb = getComb(rest, n-1);
-        const atta = comb.map(x=>[fix, ...x]);
-        res.push(...atta);
-    });
-
-    return res;
-}
-
-const makes = getComb(arr, n-2);
+let totalSum = arr.reduce((a, b) => a + b, 0); 
 let answer = Number.MAX_SAFE_INTEGER;
 
-for(let nums of makes){
-    let sum = nums.reduce((a,c)=>a+c,0);
-    const result = Math.abs(s - sum);
-    answer = Math.min(answer, result);
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+
+        let select2 = totalSum - arr[i] - arr[j];
+        let diff = Math.abs(s - select2);
+
+        answer = Math.min(answer, diff);
+    }
 }
 
 console.log(answer);
