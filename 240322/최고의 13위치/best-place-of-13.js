b3dffb7e422d306fe4ef83e75d5b6078f021283d
In Hwan KIM <dev_210@naver.com>
2024-03-22 10:07:30 +0900

    Title: 최고의 13위치, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240322/최고의 13위치/best-place-of-13.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = input.slice(1).map(x=>x.split(' ').map(Number));
+
+let answer = 0;
+for(let i = 0; i < n; i++){
+    let sum = 0;
+    for(let j = 0; j <= n-3; j++){
+        sum += arr[i][j];
+        sum += arr[i][j+1];
+        sum += arr[i][j+2];
+    }
+    
+    answer = Math.max(answer, sum);
+}
+
+console.log(answer)
