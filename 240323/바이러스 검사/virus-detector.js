e53bdebe0a347249753df0b532838b4c923b97e3
In Hwan KIM <dev_210@naver.com>
2024-03-23 08:45:52 +0900

    Title: 바이러스 검사, Time: 882ms, Memory: 95MB, Status: Passed - CodeTree

--- 240323/바이러스 검사/virus-detector.js
+++ 240323/바이러스 검사/virus-detector.js
@@ -2,23 +2,18 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const n = parseInt(input[0]);
-const c = input[1].trim().split(' ').map(BigInt);
-const [leader, member] = input[2].split(' ').map(BigInt);
+const c = input[1].trim().split(' ').map(Number);
+const [leader, member] = input[2].split(' ').map(Number);
 
-let num_leader = 0n;
-let num_member = 0n;
+let num_leader = 0;
+let num_member = 0;
 
 for(let customer of c){
     customer -= leader;
     num_leader++;
 
-    while(true){
-        if(customer <= 0){
-            break;
-        }
-
-        customer -= member;
-        num_member++;
+    if(customer > 0){
+        num_member += Math.ceil(customer/member);
     }
 }
 
