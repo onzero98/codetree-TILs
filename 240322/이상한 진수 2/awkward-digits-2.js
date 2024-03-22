7c0461aad1b42e557bd02ad6c1a02e793ebf71ad
In Hwan KIM <dev_210@naver.com>
2024-03-22 12:46:05 +0900

    Title: 이상한 진수 2, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240322/이상한 진수 2/awkward-digits-2.js
+++ 240322/이상한 진수 2/awkward-digits-2.js
@@ -4,15 +4,30 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 let a = input[0].split('');
 
 function solution(a){
+
+    if(a.length === 1){
+        return 0;
+    }
+
+    let flag = false;
+    
     for(let i = 0; i < a.length; i++){
-        if(a.length === 1){
-            return 0;
-        }
         if(a[i] === '0'){
             a[i] = '1';
             break;
         }
+        flag = true;
     }
+
+    if(flag){
+        for(let i = a.length; i > 0; i--){
+            if(a[i] === '1'){
+                a[i] = '0';
+                break;
+            }
+        }
+    }
+
     return parseInt(a.join(''), 2)
 }
 
