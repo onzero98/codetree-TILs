d230fb838bffca8a282b385b0f4013bd5dce07f4
In Hwan KIM <dev_210@naver.com>
2024-03-25 21:07:17 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240325/괄호 쌍 만들어주기 2/pair-parentheses-2.js
+++ 240325/괄호 쌍 만들어주기 2/pair-parentheses-2.js
@@ -2,28 +2,24 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const A = input[0];
-
-let index = 0;
 let count = 0;
 
-while(index < A.length){
-    let word = A.slice(index);
-    let newIndex = word.indexOf('((');
-    
-    if(newIndex < 0){
+for(let i = 0; i < A.length; i++){
+    let word = A.slice(i);
+    let ni = word.indexOf('((');
+
+    if(ni < 0)
         break;
-    } 
-
-    while(word.indexOf('))') > 0){
-        word = word.slice(word.indexOf('))')+1);
-        count++;
+    
+    for(let j = 0; j < word.length; j++){
+        let nword = word.slice(j);
+        if(nword.indexOf('))') >= 0){
+            j += nword.indexOf('))');
+            count++
+        }
     }
 
-    if(newIndex === 0){
-        index++;
-    } else {
-        index = newIndex+1;
-    }
+    i += ni;
 }
 
 console.log(count)
