60dc28a60ca8c77d0fd1b935acd8b0291499aaa6
In Hwan KIM <dev_210@naver.com>
2024-03-25 22:31:44 +0900

    Title: Carry 피하기 2, Time: 249ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240325/Carry 피하기 2/escaping-carry-2.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const nums = input.slice(1).map(Number)
+let max = 0;
+
+function select(a, b, c){
+    let arr = [a, b, c];
+    arr = arr.map(x=>x.toString().split('').reverse().map(Number));
+    let len = Math.max(...arr.map(x=>x.length));
+
+    for(let i = 0; i < len; i++){
+        let sum = 0;
+        for(let j = 0; j < 3; j++){
+            sum += arr[j][i] || 0;
+        }
+        
+        if(sum >= 10){
+            return false
+        }
+    }
+
+    return true;
+}
+
+for(let i = 0; i < n; i++){
+    for(let j = i + 1; j < n; j++){
+        for(let k = j + 1; k < n; k++){
+            if(select(nums[i], nums[j], nums[k])){
+                let sum = nums[i] + nums[j] + nums[k];
+                max = Math.max(max, sum);
+            }
+        }
+    }
+}
+
+console.log(max);
