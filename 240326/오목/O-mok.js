baffcefada24c414fc0ed52a2a58c92a24205573
In Hwan KIM <dev_210@naver.com>
2024-03-26 11:46:46 +0900

    Title: 오목, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/오목/O-mok.js
+++ 240326/오목/O-mok.js
@@ -4,48 +4,34 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const arr = input.map(line => line.split(' ').map(Number));
 const n = arr.length;
 
-const visited = Array.from(({length:n}), x=>Array(n).fill(false));
 const dir = [[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]];
 
-function countNear(i, j, arr, num){
-    let result = [[i,j]];
-    let queue = [];
-    queue.push([i, j]);
-    visited[i][j] = true;
-
-    while(queue.length > 0){
-        const [x, y] = queue.shift();
-
-        for(let [dx, dy] of dir){
-            let nx = x + dx;
-            let ny = y + dy;
-
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] === num && !visited[nx][ny]){
-                visited[nx][ny] = true;
-                queue.push([nx, ny]);
-                result.push([nx, ny]);
-            }
-        }
-    }
-
-    return result;
-}
-
-let res = 0;
-let select = 0;
 for(let i = 0; i < n; i++){
     for(let j = 0; j < n; j++){
-        if(arr[i][j] !== 0 && !visited[i][j]){
-            let count = countNear(i, j, arr, arr[i][j]);
-            if(count.length === 5){
-                res = arr[i][j];
-                select = count[2].map(x=>x+1);
+        if(arr[i][j] !== 0){
+            let color = arr[i][j];
+
+            for(let k = 0; k < dir.length; k++){
+                let count = [];
+                for(let l = 0; l < 5; l++){
+                    let nx = j + (dir[k][0] * l)
+                    let ny = i + (dir[k][1] * l)
+
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[ny][nx] === color){
+                        count.push([ny, nx]);
+                    } else {
+                        break;
+                    }
+                }
+
+                if(count.length === 5){
+                    console.log(color);
+                    console.log(count[2].map(x=>x+1).join(' '));
+                    return
+                }
             }
         }
     }
 }
 
-console.log(res)
-if(res > 0){
-    console.log(select.join(' '))
-}
+console.log(0)
