3d2f981be7eb94309913debaeabc8d50afdb2022
In Hwan KIM <dev_210@naver.com>
2024-03-29 11:09:46 +0900

    Title: 바구니 안의 사탕 2, Time: 252ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/바구니 안의 사탕 2/candy-in-the-basket-2.js
+++ 240329/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [n, k] = input[0].split(' ').map(Number);
-const Map = Array(100).fill(0);
+const Map = Array(101).fill(0);
 
 let endX = 0;
 for(let i = 1; i <= n; i++){
@@ -15,7 +15,7 @@ let answer = 0;
 for(let i = 0; i <= endX; i++){
     let candys = 0;
     for(let j = i; j < i + (2 * k) + 1; j++){
-        if(j >= 100){
+        if(j > 100){
             continue;
         }
         candys += Map[j];
