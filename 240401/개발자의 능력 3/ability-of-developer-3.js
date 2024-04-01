7b35a2e1b8bd8de66cc1e47e24dadc7c0507bf95
In Hwan KIM <dev_210@naver.com>
2024-04-01 20:22:33 +0900

    Title: 개발자의 능력 3, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240401/개발자의 능력 3/ability-of-developer-3.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const arr = input[0].split(' ').map(Number);
+
+function getComb(arr, n){
+    if(n===1)
+        return arr.map(x=>[x]);
+    
+    let res = [];
+    arr.forEach((fix, idx, ori)=>{
+        const rest = ori.slice(idx+1);
+        const comb = getComb(rest, n-1);
+        const atta = comb.map(x=>[fix, ...x]);
+        res.push(...atta);
+    });
+
+    return res;
+}
+
+let answer = Number.MAX_SAFE_INTEGER;
+let combinations = getComb(arr, 3);
+combinations.forEach((v)=>{
+    let arr1 = v;
+    let arr2 = arr.filter(x=>!arr1.includes(x));
+
+    let res1 = arr1.reduce((a,c)=>a+c,0);
+    let res2 = arr2.reduce((a,c)=>a+c,0);
+    let res = Math.abs(res1 - res2);
+    answer = Math.min(answer, res);
+});
+
+console.log(answer);
