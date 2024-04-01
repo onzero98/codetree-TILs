93ffc89c833becd92a3605fb80204abc4fc1e61f
In Hwan KIM <dev_210@naver.com>
2024-04-01 20:32:30 +0900

    Title: 개발자의 능력 3, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240401/개발자의 능력 3/ability-of-developer-3.js
+++ 240401/개발자의 능력 3/ability-of-developer-3.js
@@ -22,7 +22,14 @@ let answer = Number.MAX_SAFE_INTEGER;
 let combinations = getComb(arr, 3);
 combinations.forEach((v)=>{
     let arr1 = v;
-    let arr2 = arr.filter(x=>!arr1.includes(x));
+    let arr2 = arr.slice();
+    
+    arr1.forEach(item => {
+        const index = arr2.indexOf(item);
+        if (index > -1) {
+            arr2.splice(index, 1);
+        }
+    });
 
     let res1 = arr1.reduce((a,c)=>a+c,0);
     let res2 = arr2.reduce((a,c)=>a+c,0);
