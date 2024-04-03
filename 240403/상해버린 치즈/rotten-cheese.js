1f8078454d1119947cf4eec4c4ef540c48d26083
In Hwan KIM <dev_210@naver.com>
2024-04-03 13:49:11 +0900

    Title: 상해버린 치즈, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240403/상해버린 치즈/rotten-cheese.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m, d, s] = input[0].split(' ').map(Number);
+const eatArr = input.slice(1, d+1).map(lines => lines.split(' ').map(Number));
+const res = input.slice(d+1).map(lines => lines.split(' ').map(Number));
+const sickTimeArr = Array(n).fill(0);
+
+for(let [p, t] of res){
+    sickTimeArr[p-1] = t;
+}
+
+let answer = 0;
+
+for(let i = 1; i <= m; i++){ // 치즈만큼 반복
+    let eatCheeze = [];
+    for(let j = 0; j < d; j++){
+        const [p, m, t] = eatArr[j];
+
+        if(i === m){
+            eatCheeze.push([p, t]);
+        }
+    }
+
+    let flag = true;
+    for(let [p, t] of eatCheeze){
+        if(sickTimeArr[p-1] === 0)
+            continue;
+        
+        if(sickTimeArr[p-1] < t){
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        answer = Math.max(answer, eatCheeze.length);
+    }
+}
+
+console.log(answer)
