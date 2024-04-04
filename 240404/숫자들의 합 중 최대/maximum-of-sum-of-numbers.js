1da8e122bbf3f98b5e845ee2943fff9ba6f37011
In Hwan KIM <dev_210@naver.com>
2024-04-04 10:24:43 +0900

    Title: 숫자들의 합 중 최대, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

--- 240404/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
+++ 240404/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
@@ -3,14 +3,17 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [x, y] = input[0].split(' ').map(Number);
 
+function dsum(n){
+    if(n < 10){
+        return n;
+    } else {
+        return dsum(Math.floor(n/10)) + (n % 10);
+    }
+}
+
 let answer = 0;
 for(let i = x; i <= y; i++){
-    let num = i.toString().split('').map(Number);
-    let sum = 0;
-    for(let n of num){
-        sum += n;
-    }
-    answer = Math.max(answer, sum);
+    answer = Math.max(answer, dsum(i));
 }
 
 console.log(answer);
