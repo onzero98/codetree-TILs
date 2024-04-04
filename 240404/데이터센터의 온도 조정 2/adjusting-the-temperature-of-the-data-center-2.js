80bfa56f980ac0cb95ebb4349074ada15a7cd478
In Hwan KIM <dev_210@naver.com>
2024-04-04 22:41:10 +0900

    Title: 데이터센터의 온도 조정 2, Time: 225ms, Memory: 18MB, Status: Passed - CodeTree

--- 240404/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
+++ 240404/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
@@ -3,24 +3,29 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [n, c, g, h] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map(lines => lines.split(' ').map(Number));
+const MAX_T = 1000;
+
+function efficiency(low, high, t) {
+    if (t < low) {
+        return c;
+    } else if (t <= high) {
+        return g;
+    } else {
+        return h;
+    }
+}
+
+function performance(t) {
+    let total = 0;
+    for (let i = 0; i < n; i++) {
+        total += efficiency(arr[i][0], arr[i][1], t);
+    }
+    return total;
+}
 
 let answer = 0;
-for(let i = 0; i < n; i++){
-    const [ta, tb] = arr[i];
-    let score = 0;
-    for(let j = 0; j < n; j++){
-        const [nta, ntb] = arr[j];
-            
-        if(nta <= tb && nta >= ta){
-            score += g;
-        } else if(ntb < ta) {
-            score += c;
-        } else if(tb < nta){
-            score += h;
-        }
-    }
-    
-    answer = Math.max(answer, score);
+for (let t = 0; t <= MAX_T; t++) {
+    answer = Math.max(answer, performance(t));
 }
 
 console.log(answer)
