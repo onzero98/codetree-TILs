190003b28ba72a397d579f3fae9f44ff5fe0d194
In Hwan KIM <dev_210@naver.com>
2024-04-05 14:58:22 +0900

    Title: 정보에 따른 숫자 2, Time: 220ms, Memory: 14MB, Status: Passed - CodeTree

--- 240405/정보에 따른 숫자 2/number-based-on-information-2.js
+++ 240405/정보에 따른 숫자 2/number-based-on-information-2.js
@@ -3,24 +3,37 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [t, a, b] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map(lines => lines.split(' ').map((v,i)=> i===1 ? parseInt(v): v));
-const map = Array(1001).fill(false);
-arr.forEach((v)=>{
-    const [c, x] = v;
-    map[x] = c;
+
+const sP = [];
+const nP = [];
+
+arr.forEach(([c, x])=>{
+    if (c === 'S') {
+        sP.push(x);
+    } else {
+        nP.push(x);
+    }
 });
+
+sP.sort((a,b)=>a-b);
+nP.sort((a,b)=>a-b);
+
+function find(arr, x){
+    let minDiff = Number.MAX_SAFE_INTEGER;
+    arr.forEach(v => {
+        const curDiff = Math.abs(v - x);
+        minDiff = Math.min(minDiff, curDiff);
+    })
+
+    return minDiff;
+}
 
 let answer = 0;
 for(let i = a; i <= b; i++){
-    const si = map.indexOf('S');
-    const ni = map.indexOf('N') === -1 ? Number.MAX_SAFE_INTEGER : map.indexOf('N');
+    const closesP = find(sP, i);
+    const closenP = find(nP, i);
 
-    if(i !== a && i === si){
-        map[ni] = false;
-    }
-    if(i === ni){
-        map[si] = false;    
-    }
-    if(Math.abs(si - i) <= Math.abs(ni - i)){
+    if(closesP <= closenP){
         answer++;
     }
 }
