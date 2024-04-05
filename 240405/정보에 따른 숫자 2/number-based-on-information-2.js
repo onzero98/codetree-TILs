b25442cbc43cf2810f7f6084a97d6b38ca83ee36
In Hwan KIM <dev_210@naver.com>
2024-04-05 14:19:26 +0900

    Title: 정보에 따른 숫자 2, Time: 195ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240405/정보에 따른 숫자 2/number-based-on-information-2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [t, a, b] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(lines => lines.split(' ').map((v,i)=> i===1 ? parseInt(v): v));
+const map = Array(1001).fill(false);
+arr.forEach((v)=>{
+    const [c, x] = v;
+    map[x] = c;
+});
+
+let answer = 0;
+for(let i = a; i <= b; i++){
+    const si = map.indexOf('S');
+    const ni = map.indexOf('N');
+
+    if(i !== a && i === si){
+        map[ni] = false;
+    }
+    if(i === ni){
+        map[si] = false;    
+    }
+    if(Math.abs(si - i) <= Math.abs(ni - i)){
+        answer++;
+    }
+}
+
+console.log(answer);
