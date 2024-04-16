c7bb29082637dfc9c12de8e9494bf8d5f1dd934b
In Hwan KIM <dev_210@naver.com>
2024-04-16 15:33:54 +0900

    Title: 좌표평면 위의 균형 2, Time: 235ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240416/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = input.slice(1).map(lines => lines.split(' ').map(Number));
+
+let maxX = 0;
+let maxY = 0;
+arr.forEach(([x, y])=>{
+    maxX = Math.max(maxX, x);
+    maxY = Math.max(maxY, y);
+});
+
+let m = Number.MAX_SAFE_INTEGER;
+
+for(let i = 2; i < maxX; i += 2){
+    for(let j = 2; j < maxY; j += 2){
+        let plot = [0, 0, 0, 0];
+
+        for(let [x, y] of arr){
+            if(x < i && y > j){
+                plot[0] += 1;
+            } else if(x > i && y > j){
+                plot[1] += 1;
+            } else if(x > i && y < j){
+                plot[2] += 1;
+            } else if(x < i && y < j){
+                plot[3] += 1;
+            }
+        }
+
+        m = Math.min(m, Math.max(...plot));
+    }
+}
+
+console.log(m);
