f6ea08536bd8c7a46f1a26051933f8a4675dafd5
In Hwan KIM <dev_210@naver.com>
2024-04-16 17:38:30 +0900

    Title: 팀으로 하는 틱택토 2, Time: 287ms, Memory: 14MB, Status: Passed - CodeTree

--- 240416/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
+++ 240416/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.js
@@ -4,11 +4,13 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const arr = input.map(lines => lines.split('').map(Number));
 
 let dir = [[-1, 1], [0, 1], [1, 1], [1, 0]];
+const numsArr = [];
+
 let answer = 0;
 for(let i = 0; i < 3; i++){
     for(let j = 0; j < 3; j++){
         for(let [y, x] of dir){
-            const nums = [];
+            let num = '';
             const nx = j + 2 * x;
             const ny = i + 2 * y;
 
@@ -16,15 +18,21 @@ for(let i = 0; i < 3; i++){
                 continue;
             }
 
-            nums.push(arr[i][j])
-            nums.push(arr[i+y][j+x])
-            nums.push(arr[i+2*y][j+2*x])
-            
-            const setNums = new Set(nums);
-            if(setNums.size === 2){
-                answer++;
-            }
+            num += arr[i][j];
+            num += arr[i+y][j+x];
+            num += arr[i+2*y][j+2*x];
+
+            numsArr.push(num);
         }
+    }
+}
+
+const filtered = new Set(numsArr);
+for(let num of filtered){
+    const nums = num.split('').map(Number);
+    const setNums = new Set(nums);
+    if(setNums.size === 2){
+        answer++;
     }
 }
 
