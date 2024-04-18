0bc6f871c4894c16397d480509561f8701a51367
In Hwan KIM <dev_210@naver.com>
2024-04-18 15:58:48 +0900

    Title: 숫자들의 최대 차, Time: 242ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240418/숫자들의 최대 차/maximum-difference-in-numbers.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(Number).sort((a,b)=>a-b);
+
+let answer = 0;
+
+for(let i = 0; i < n; i++){
+    let s = arr[i];
+    let e = arr[i] + k;
+    let count = 0;
+
+    for(let j = i; j < n; j++){
+        if(s <= j && j <= e){
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    answer = Math.max(answer, count);
+}
+
+console.log(answer)
