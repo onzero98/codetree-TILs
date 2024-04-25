86a77075f36a0508d9bb59edb8df4d65c591d9b4
In Hwan KIM <dev_210@naver.com>
2024-04-25 11:11:12 +0900

    Title: 최대 H 점수 2, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240425/최대 H 점수 2/maximum-h-score-2.js
+++ 240425/최대 H 점수 2/maximum-h-score-2.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+const MAX = 100;
 const [n, l] = input[0].split(' ').map(Number);
 let arr = input[1].split(' ').map(Number);
 arr = arr.sort((a,b)=>a-b);
 
-for(let i = 0; i < l; i++){
-    arr[i] += 1;
-}
-
 let answer = 0;
-for(let item of arr){
-    const filtered = arr.filter((v)=> v >= item).length;
-    if(filtered >= item){
-        answer = Math.max(answer, item);
+for(let i = 0; i <= n - l; i++){
+    let tmp = arr.slice();
+    
+    for(let j = i; j < i + l; j++){
+        tmp[j] += 1;
+    }
+
+    const nums = new Set(tmp);
+    for(let num of nums){
+        const filter = tmp.filter(x=>x>=num).length;
+        if(num <= filter){
+            answer = Math.max(answer, num);
+        }
     }
 }
 
