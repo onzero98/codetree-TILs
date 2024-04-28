316009cf60e69e3871fc1156760c464f7334a980
In Hwan KIM <dev_210@naver.com>
2024-04-29 08:58:11 +0900

    Title: 두 선분, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240429/두 선분/two-lines.js
+++ 240429/두 선분/two-lines.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [x1, x2, x3, x4] = input[0].split(' ').map(Number);
 
-if(x2 >= x3){
+if(x2 >= x3 || x2 >= x4){
     console.log('intersecting')
 } else {
     console.log('nonintersecting')
