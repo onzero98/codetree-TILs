3893acc78b6687c13d47dddb94b2af3d68c50916
In Hwan KIM <dev_210@naver.com>
2024-06-04 13:13:32 +0900

    Title: X 달리기, Time: 5089ms, Memory: 78MB, Status: Time Limit Exceed - CodeTree

--- 240604/X 달리기/x-run.js
+++ 240604/X 달리기/x-run.js
@@ -5,6 +5,8 @@ const X = parseInt(input[0]);
 
 function minTimeToReachX(X) {
     const queue = [[1, 1, 1]];
+    const visited = new Set();
+    visited.add('0,1');
     
     while (queue.length > 0) {
         const [dist, speed, time] = queue.shift();
@@ -20,8 +22,9 @@ function minTimeToReachX(X) {
         ];
         
         for (const [nextDist, nextSpeed, nextTime] of nextStates) {
-            if (nextSpeed > 0) {
+            if (nextSpeed > 0 && !visited.has(`${nextDist},${nextSpeed}`)) {
                 queue.push([nextDist, nextSpeed, nextTime]);
+                visited.add(`${nextDist},${nextSpeed}`);
             }
         }
     }
