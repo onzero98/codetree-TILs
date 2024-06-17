e6268ce06add6642876699910326fe33298725f8
In Hwan KIM <dev_210@naver.com>
2024-06-17 19:01:10 +0900

    Title: 순 간 이 동, Time: 203ms, Memory: 47MB, Status: Passed - CodeTree

--- 240617/순 간 이 동/teleportation.js
+++ 240617/순 간 이 동/teleportation.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
 
-const [a, b, x, y] = input[0].split(' ').map(Number);
+const [a, b, x, y] = input.split(' ').map(Number);
 
 let min = Number.MAX_SAFE_INTEGER;
 
