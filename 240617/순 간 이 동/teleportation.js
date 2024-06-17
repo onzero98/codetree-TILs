d8a83c03d364d195bf78bd32ed58295504f3d155
In Hwan KIM <dev_210@naver.com>
2024-06-17 19:00:14 +0900

    Title: 순 간 이 동, Time: 203ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240617/순 간 이 동/teleportation.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const [a, b, x, y] = input[0].split(' ').map(Number);
+
+let min = Number.MAX_SAFE_INTEGER;
+
+min = Math.min(min, Math.abs(b - a));
+min = Math.min(min, Math.abs(x - a) + Math.abs(b - y));
+min = Math.min(min, Math.abs(y - a) + Math.abs(b - x));
+
+console.log(min);
