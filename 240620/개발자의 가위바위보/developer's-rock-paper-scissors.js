f997df654a35a794c7cbcb9e1a5a65c20cb641bf
In Hwan KIM <dev_210@naver.com>
2024-06-20 15:10:11 +0900

    Title: 개발자의 가위바위보, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240620/개발자의 가위바위보/developer's-rock-paper-scissors.js
+++ 240620/개발자의 가위바위보/developer's-rock-paper-scissors.js
@@ -11,7 +11,7 @@ for(let i = 1; i < input.length; i++){
     }
 
     if((dev1 === 1 && dev2 === 3) || (dev1 === 3 && dev2 === 1)){
-        dev1 < dev2 ? CASE[1] += 1 : CASE[1] -= 1;
+        dev1 < dev2 ? CASE[1] -= 1 : CASE[1] += 1;
     }
     
     if((dev1 === 2 && dev2 === 3) || (dev1 === 3 && dev2 === 2)){
@@ -20,5 +20,4 @@ for(let i = 1; i < input.length; i++){
 }
 
 CASE.sort().reverse();
-
 console.log(CASE[0]+CASE[1]);
