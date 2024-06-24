814b02f866405b02069e552d9e172186dd92de5b
In Hwan KIM <dev_210@naver.com>
2024-06-24 14:18:28 +0900

    Title: 연속된 숫자 만들기 3, Time: 215ms, Memory: 15MB, Status: Passed - CodeTree

--- 240624/연속된 숫자 만들기 3/create-consecutive-numbers-3.js
+++ 240624/연속된 숫자 만들기 3/create-consecutive-numbers-3.js
@@ -2,25 +2,17 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const arr = input[0].split(' ').map(Number);
-let result = 0;
 
-function solution(array) {
-    let [st, nd, rd] = array;
-    let count = 0;
-
-    while (st + 1 < nd || nd + 1 < rd) {
-        if (st + 1 < nd) {
-            st = st + 1;
-            count++;
-        }
-        if (nd + 1 < rd) {
-            nd = nd - 1;
-            count++;
-        }
-    }
-
-    result = count;
+if (arr[0] + 1 === arr[1] && arr[1] + 1 === arr[2]) {
+    console.log(0);
+    return;
 }
 
-solution(arr);
-console.log(result);
+let max = 0;
+let move = arr[2] - arr[1] - 1;
+max = Math.max(max, move);
+
+move = arr[1] - arr[0] - 1;
+max = Math.max(max, move);
+
+console.log(max);
