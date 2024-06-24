cd9f872fc6523cebc819617d902c834713a20010
In Hwan KIM <dev_210@naver.com>
2024-06-24 14:25:07 +0900

    Title: 독서실의 거리두기 2, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240624/독서실의 거리두기 2/study-cafe-keeping-distance-2.js
+++ 240624/독서실의 거리두기 2/study-cafe-keeping-distance-2.js
@@ -3,14 +3,11 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = parseInt(input[0]);
 
-let temp = input[1].split('1').map(x=>x.length)
-delete temp[0];
-delete temp[temp.length-1];
-const arr = temp.slice().filter(x => x !== null)
+let arr = input[1].split('1').filter(x=>x.length > 0).map(x=>x.length);
 
 const max = Math.max(...arr);
-const min = Math.min(...arr) + 1;
+const min = Math.min(...arr);
 
 const value = Math.ceil(max/2);
-const answer = Math.min(min, value);
+const answer = Math.max(min, value);
 console.log(answer);
